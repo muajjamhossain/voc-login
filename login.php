f2232c29b7979e4eadc92457091e8303db17800f
DESKTOP-9HF67QV\muajj <muajjam.imu@gmail.com>
2024-05-20 23:14:38 +0600

    host dynamic

--- login.php
+++ login.php
@@ -1,6 +1,41 @@
 <?php
-// Check if user ID and password are provided
-if (isset($_POST['emp_id']) && isset($_POST['password'])) {
+// Check if user ID, password, and channel ID are provided
+
+// Determine the protocol
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+
+
+  // Get the host name
+//   $host = $_SERVER['HTTP_HOST'];
+
+  $host = $_SERVER['HTTP_HOST'] ?? 'localhost'; // Default to 'localhost' if not set
+
+    $hostMappings = [
+        '192.168.45.22:5306' => 'http://192.168.45.22:5306/api/loginAPI',
+        '124.109.105.40:5306' => 'http://124.109.105.40:5306/api/loginAPI',
+        'localhost' => 'http://mtb-clone.test/api/loginAPI'
+    ];
+
+    if (isset($hostMappings[$host])) {
+        $apiUrl = $hostMappings[$host];
+    } else {
+        echo "This is not a valid request!";
+        die();
+    }
+
+//   print_r($host);
+//   print_r($apiUrl);
+//   die();
+ 
+  // Get the directory path
+  $scriptName = $_SERVER['SCRIPT_NAME'];
+  $path = dirname($scriptName);
+  
+  // Construct the base URL
+  $baseUrl = $protocol . $host . rtrim($path, '/');
+ 
+
+if (isset($_POST['emp_id']) && isset($_POST['password']) && isset($_POST['channel_id'])) {
 
     $postData = http_build_query(array(
         'emp_id' => $_POST['emp_id'],
@@ -15,17 +50,24 @@ if (isset($_POST['emp_id']) && isset($_POST['password'])) {
         'mobilephone' => $_POST['mobilephone'],
         'office_type' => $_POST['office_type'],
         'username_or_email' => $_POST['emp_id'],
-        'password' => $_POST['password']
-
+        'channel_id' => $_POST['channel_id'],
+        'base_url' => $_POST['base_url'],
     ));
 
+     // Set headers
+    $headers = array(
+        'password: ' . $_POST['password'],
+        
+    );
+
     // Set cURL options
-   
     $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, 'http://mtb-clone.test/api/loginAPI');
+    curl_setopt($curl, CURLOPT_URL, $apiUrl );
     curl_setopt($curl, CURLOPT_POST, true);
     curl_setopt($curl, CURLOPT_POSTFIELDS, $postData);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, false);
+
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
     $response = curl_exec($curl);
 
@@ -39,6 +81,6 @@ if (isset($_POST['emp_id']) && isset($_POST['password'])) {
     var_dump($response);
     die();
 } else {
-    echo "User ID and password are required!";
+    echo "User ID, password, and channel ID are required!";
 }
 ?>
